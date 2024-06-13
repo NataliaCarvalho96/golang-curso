a4b67ad1fc35c50fd98e50266b3eb9fa8ca1b96f
NCA <nataliacalves.22@gmail.com>
2024-06-13 14:37:41 -0300

    Aula de Pacotes
     Add conteúdo sobre Pacotes em GO.

---
+++ 1_Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,10 @@
+package auxiliar
+
+import(
+	"fmt"
+)
+//Escrever registra uma mensagaem na tela
+func Escrever(){
+	fmt.Println("Escrevendo do arquivo auxiliar")
+	escrever2()
+}
---
+++ 1_Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+//Escreve do segundo pacote auxiliar
+func escrever2() {
+    fmt.Println("Escrevendo pela função escrever2")
+}
