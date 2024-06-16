5c2c751b3c7495f65695024a2fe899adda94bf6b
NCA <nataliacalves.22@gmail.com>
2024-06-16 20:40:18 -0300

    Loops
    Breve resumo sobre Loops.

---
+++ 13_Loops/loops.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+    "time"
+)
+
+func main() {
+   // i := 0
+	 //for i < 10 {
+		//i++
+		//fmt.Println("Incrementando i") //printou varias vezes na tela enquanto menor que 10
+        //time.Sleep(time.Second)
+        
+   // }
+	//fmt.Println(i)
+
+	for j := 0; j < 10; j++ {
+		fmt.Println("Incrementando j", j)
+        time.Sleep(time.Second)
+    
+
+	}
+}
